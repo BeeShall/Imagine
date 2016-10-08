1cbfd261b2e335e308daa9bd8321fedc800e6f4a
source09 <will.santosh@gmail.com>
2016-10-08 02:04:12 -0400

    Post request handled

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
 from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
+	if request.method=='POST':
+		user_input=request.form['user_tag']
+		return render_template('form.html',message='Input Received')
+
 	return render_template('form.html')
+		
 
 
 
